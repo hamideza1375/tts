a5ccab6ef125fae53883311d9b5f3395a1fceefa
hamideza1375 <84849675+hamideza1375@users.noreply.github.com>
2023-07-12 23:48:23 +0330

    new

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import { createStackNavigator, createAppContainer } from 'react-navigation';
 import Home from './index';
 const Router = createAppContainer(createStackNavigator({Home}));
-export default function App() { return <Router /> }
+
+const App = () => <Router />
+
+export default App
+// a
